08c85675d40f26747e86aff920712bc6bff8a813
eliecerecology <eliecerecology@gmail.com>
2018-07-11 15:31:34 +0300

    rem

--- deeplearning.R
+++ deeplearning.R
@@ -7,23 +7,30 @@ library(keras)
 data <- read.csv("Cardiotocographic.csv", header = T)
 str(data)
 dim(data)
+edit(data)
+
+#######################
 # Change to matrix
+######################
 data <- as.matrix(data)
 dimnames(data) <- NULL #now does not have names
 
 # Normalize
-
 data[,1:21] <- normalize(data[,1:21])
 data[,22] <- as.numeric(data[,22]) - 1 #values 0 1 2
 summary(data)
 
+######################
 # Data partition
+######################
 set.seed(1234)
 ind <- sample(2, nrow(data), replace = T, prob = c(0.7, 0.3))
+
 training <- data[ind==1, 1:21]
 test <- data[ind==2, 1:21]
 dim(training)
 (1523/100)*80
+
 ##########FINAL forrmat
 trainingtarget <- data[ind == 1, 22]
 testtarget <- data[ind == 2, 22]
@@ -38,8 +45,21 @@ print(trainLabels)
 model <- keras_model_sequential()
 model %>%
          layer_dense(units = 50, activation = 'relu', input_shape = c(21)) %>%
-         layer_dense(units = 3, activation = "sigmoid")
+         layer_dense(units = 3, activation = "sigmoid") #softmax
 summary(model)
+
+##############################################
+input * nodes + nodes
+
+#Param layer 1 ==> 21 * 50  1 layers
+#21*50 + 50 = 1100
+##############################################
+#Param layer 2 ==> 21 * 3
+#(50 * 3) + 3 # ==> 153
+##############################################
+
+
+
 
 ####################
 # Compile
@@ -66,9 +86,9 @@ model1 <- model %>%
 
 model1
 #$loss
-#[1] 0.4124508
+#[1] 0.3924508
 #$acc
-#[1] 0.8640133
+#[1] 0.8740133
 
 #####################PREDICTIONS
 # Prediction & confusion matrix - test data
@@ -87,7 +107,7 @@ cbind(prob, pred, testtarget)
 #######################
 model2 <- keras_model_sequential()
 model2 %>%
-         layer_dense(units = 50, activation = 'relu', input_shape = c(21)) %>%
+         layer_dense(units = 50, activation = 'elu', input_shape = c(21)) %>%
          layer_dense(units = 3, activation = "sigmoid")
 
 summary(model2)
@@ -106,7 +126,6 @@ history2 <- model2 %>%
              batch_size = 32,
              validation_split = 0.2)
 plot(history2)
-
 #####################PREDICTIONS
 # Prediction & confusion matrix - test data
 prob2 <- model2 %>%
@@ -118,6 +137,7 @@ pred2 <- model2 %>%
 table2 <- table(Predicted = pred2, Actual = testtarget)
 table1
 cbind(prob, pred, testtarget)
+
 
 ########################
 # Fine-tune model 3
@@ -160,3 +180,130 @@ table2
 table3
 cbind(prob, pred, testtarget)
 
+########################
+# Fine-tune model 4
+########################
+model4 <- keras_model_sequential()
+
+model4 %>%
+         layer_dense(units = 50, activation = 'relu', input_shape = c(21)) %>%
+         layer_dense(units = 8, activation = 'relu') %>%
+         layer_dense(units = 3, activation = "softmax")
+
+summary(model4)
+
+# Compile
+model4 %>%
+         compile(loss = "categorical_crossentropy",
+                 optimizer = "adam",
+                 metrics = "accuracy")
+
+# Fit model
+history4 <- model4 %>%
+         fit(training,
+             trainLabels,
+             epoch = 200,
+             batch_size = 32,
+             validation_split = 0.2)
+plot(history4)
+
+#####################PREDICTIONS
+# Prediction & confusion matrix - test data
+prob4 <- model4 %>%
+         predict_proba(test)
+
+pred4 <- model4 %>%
+         predict_classes(test)
+
+table4 <- table(Predicted = pred4, Actual = testtarget)
+table1
+table2
+table3
+table4
+cbind(prob, pred, testtarget)
+
+########################
+# Fine-tune model 5
+########################
+model5 <- keras_model_sequential()
+
+model5 %>%
+         layer_dense(units = 50, activation = 'elu', input_shape = c(21)) %>%
+         layer_dense(units = 8, activation = 'elu') %>%
+         layer_dense(units = 3, activation = "softmax")
+
+summary(model5)
+
+# Compile
+model5 %>%
+         compile(loss = "categorical_crossentropy",
+                 optimizer = "adam",
+                 metrics = "accuracy")
+
+# Fit model
+history5 <- model5 %>%
+         fit(training,
+             trainLabels,
+             epoch = 400,
+             batch_size = 32,
+             validation_split = 0.2)
+plot(history5)
+
+#####################PREDICTIONS
+# Prediction & confusion matrix - test data
+prob5 <- model5 %>%
+         predict_proba(test)
+
+pred5 <- model5 %>%
+         predict_classes(test)
+
+table5 <- table(Predicted = pred5, Actual = testtarget)
+table1
+table2
+table3
+table4
+table5
+cbind(prob, pred, testtarget)
+
+########################
+# Fine-tune model 6
+########################
+model6 <- keras_model_sequential()
+
+model6 %>%
+         layer_dense(units = 50, activation = 'elu', input_shape = c(21)) %>%
+         layer_dense(units = 8, activation = 'elu') %>%
+         layer_dense(units = 3, activation = "softmax")
+
+summary(model6)
+
+# Compile
+model6 %>%
+         compile(loss = "categorical_crossentropy",
+                 optimizer = optimizer_rmsprop(lr = 2e-5),
+                 metrics = "accuracy")
+
+# Fit model
+history6 <- model6 %>%
+         fit(training,
+             trainLabels,
+             epoch = 75,
+             batch_size = 32,
+             validation_split = 0.2)
+plot(history6)
+
+#####################PREDICTIONS
+# Prediction & confusion matrix - test data
+prob6 <- model6 %>%
+         predict_proba(test)
+
+pred6 <- model6 %>%
+         predict_classes(test)
+
+table6 <- table(Predicted = pred6, Actual = testtarget)
+table1
+table2
+table3
+table4
+table5
+cbind(prob, pred, testtarget)
